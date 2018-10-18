9513907ebc5c081ab4dfc97b582ce11469413c68
sdk1270 <sdk1270@gmail.com>
2018-10-18 16:57:18 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,7 +14,8 @@ my_two_array = ["element a", "element b"]
 ##############
 
 #first_element
-taylor_swift = 
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+taylor_swift[0]
 #third_element
 
 #last_element
