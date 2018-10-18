23ed3f1ec6acd63ff97e5e48496756447d5ad7c8
sdk1270 <sdk1270@gmail.com>
2018-10-18 17:05:30 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,47 +1,31 @@
-###################
-## instantiating ##
-###################
+def instaniate_new_array
+	Array.new
+end
 
-#instantiate_new_array
-instantiate_new_array = []
+def array_with_two_elements
+	[1, 2]
+end
 
-#array_with_two_elements
+def first_element(array)
+	array[0]
+end
 
-my_two_array = ["element a", "element b"]
+def third_element(array)
+	array[2]
+end
 
-##############
-## indexing ##
-##############
+def last_element(array)
+	array[-1]
+end
 
-#first_element
+def first_element_with_array_methods(array)
+	array.first
+end
 
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-taylor_swift[0]
+def last_element_with_array_methods(array)
+	array.last
+end
 
-#third_element
-
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-taylor_swift[2]
-
-#last_element
-
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-taylor_swift[-1] 
-
-###################
-## array methods ##
-###################
-
-#first_element_with_array_methods
-
-south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-south_east_asia.first 
-
-#last_element_with_array_methods
-south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-south_east_asia.last
-
-
-#length_of_array
-south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-south_east_asia.length 
+def length_of_array(array)
+	array.size
+end
