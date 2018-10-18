d20a1a575bff3a958ebe031761214ef77fbe6d62
sdk1270 <sdk1270@gmail.com>
2018-10-18 16:58:35 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -22,7 +22,11 @@ taylor_swift[0]
 
 taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 taylor_swift[2]
+
 #last_element
+
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+taylor_swift.last 
 
 ###################
 ## array methods ##
