ceb3cd90d9225ffdb9532dd477947a721329fba2
sdk1270 <sdk1270@gmail.com>
2018-10-18 16:51:44 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,30 @@
+###################
+## instantiating ##
+###################
+
+#instantiate_new_array
+array = []
+
+#array_with_two_elements
+
+array = []
+
+##############
+## indexing ##
+##############
+
+#first_element
+
+#third_element
+
+#last_element
+
+###################
+## array methods ##
+###################
+
+#first_element_with_array_methods
+
+#last_element_with_array_methods
+
+#length_of_array
